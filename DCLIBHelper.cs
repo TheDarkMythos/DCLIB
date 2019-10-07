722aa72ceba4c1a8c7b3ba29289b14e64cf72c1a
boldenis44 <55795847+boldenis44@users.noreply.github.com>
2019-10-07 16:43:30 +0700

    Renaming

--- DCLIBHelper.cs
+++ DCLIBHelper.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DCLIB
+public class DCLIB
 {
-    public class Class1
+    public void GetConfigFile(string Name)
     {
+
     }
 }
+

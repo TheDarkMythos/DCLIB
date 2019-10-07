ce74b7bce50a11f774820e85adf0701578a160fa
boldenis44 <55795847+boldenis44@users.noreply.github.com>
2019-10-07 17:26:04 +0700

    Update DCLIBHelper.cs

--- DCLIBHelper.cs
+++ DCLIBHelper.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 public class DCLIB
 {
-    public void GetConfigFile(string Name)
+    public string GetConfigFile(string Name)
     {
+        if (File.Exists(Assembly.GetEntryAssembly().Location.Substring(0, Assembly.GetEntryAssembly().Location.LastIndexOf(@"\") + 1) + Name))
+        {
+            return File.ReadAllText(Assembly.GetEntryAssembly().Location.Substring(0, Assembly.GetEntryAssembly().Location.LastIndexOf(@"\") + 1) + Name);
+        }
+        else
+        {
+            try
+            {
+                var assembly = Assembly.GetEntryAssembly();
 
+                var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(Name));
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
 
